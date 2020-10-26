7e38de5605e9a5aa3dc5e6b80142516e6f98f1a8
Vladimir <vladkodoktorov@gmail.com>
2020-10-26 18:32:26 +0000

    fixed bug with extra spacing

--- load-csv.js
+++ load-csv.js
@@ -1,10 +1,36 @@
 const fs = require("fs");
 const _ = require("lodash");
 
-function loadCSV(filename, {converters = {}}) {
+function extractColumns(data, columnNames){
+    const headers = _.first(data);
+    const indexes = _.map(columnNames, column => headers.indexOf(column));
+    const extracted = _.map(data, row => _.pullAt(row, indexes));
+
+    return extracted;
+}
+
+function loadCSV(filename,
+     {converters = {},
+      dataColumns = [], 
+      labelColumns = []
+    }) {
+        console.log(dataColumns);
   let data = fs.readFileSync(filename, { encoding: "utf-8" });
-  data = data.split("\n").map((row) => row.split(","));
-  data = data.map((row) => _.dropRightWhile(row, (val) => val === "")); // drop extra column in every row start from the right
+  data = _.map(data.split('\n'), row => row.split(','));
+  console.log(data)
+//this fucking bullshit loop is necessary because for some reason there is a \r after last column in my csv file
+  for(var i = 0; i < data.length; i++){
+    console.log(data[i]);
+    for(var j = 0; j < data[i].length; j++){
+        data[i][j] = data[i][j].replace(/\n|\r/g, "");
+        console.log(data[i][j]);
+    }
+}
+  console.log(data)
+ // drop extra column in every row start from the right
+  data = _.dropRightWhile(data, val => _.isEqual(val, ['']));
+  data = _.dropRightWhile(data, val => _.includes('\r', val));
+;
   const headers = _.first(data);
 
   data = data.map((row, index) => {
@@ -21,9 +47,19 @@ function loadCSV(filename, {converters = {}}) {
      return _.isNaN(result) ? element : result;
     });
   });
+
+  let labels = extractColumns(data, labelColumns);
+  data = extractColumns(data, dataColumns);
+
+  data.shift();
+  labels.shift();
   console.log(data);
+  console.log(labels);
+
 }
-loadCSV("data.csv", {  
+loadCSV("data.csv", { 
+    dataColumns : ['height','value'],
+    labelColumns: ['passed'],
     converters : {
         passed: val => val === 'TRUE' ? true : false  //I can use custom logic here to convert values 
     }
